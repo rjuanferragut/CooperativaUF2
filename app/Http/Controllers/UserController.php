fddc4455e9caf9e7fba91e80454868245edb17a2
rafa <rjuanferragut@gmail.com>
2020-06-05 13:41:17 +0200

    update users and show all users Done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\User;
 
 /*
 |--------------------------------------------------------------------------
